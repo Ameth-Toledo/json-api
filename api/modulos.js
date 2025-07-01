014a19667d25b1b569c22e89a0a7355e510c64d5
Ameth-Toledo <233363@ids.upchiapas.edu.mx>
2025-06-30 23:18:40 -0600

    Fix modulos endpoint

--- api/modulos.js
+++ api/modulos.js
@@ -1,6 +1,10 @@
 import modulo from '../modulos.json';
 
 export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
   const { id_curso, titulo } = req.query;
 
   let resultados = modulo;
